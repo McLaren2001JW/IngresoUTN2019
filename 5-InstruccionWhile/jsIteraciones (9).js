899907a435c35906f7d973e934402cdcb7e7717b
McLaren2001JW <47321450+McLaren2001JW@users.noreply.github.com>
2019-02-23 13:06:36 -0300

    Add files via upload
    INTENTO 2

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,18 +1,62 @@
 function Mostrar()
 {
 
-	var contador=0;
-	// declarar variables
-	
-	var respuesta='si';
+	var contador = 0;
+	var numero;
+	var maximo;
+	var minimo;
+	var cantidad = 0
+	var respuesta ='si';
 
 	while(respuesta!='no')
 	{
-		
+		numero = prompt("ingrese un numero");
+		if(cantidad == 0)
+			{
+				maximo = numero;
+			    minimo = numero;
+			}
+			else
+			{
+			if (numero > maximo)
+
+		{
+			maximo = numero;
+		}
+		if (numero<minimo)
+		{
+			minimo = numero
+		}
 	
 	}
+	cantidad ++;
+	respuesta = prompt("desea ingresar otro numero");
+	document.getElementById("minimo").value= minimo;
+	document.getElementById("maximo").value= minimo; 
+	
+}
 
 
+var contador = 0;
+	var numero;
+	var maximo;
+	var minimo;
+	var cantidad = 0
+	var respuesta ='si';
+	var flag = flase;
+
+while(respuesta!='no')
+	{
+		numero = prompt("ingrese un numero");
+		if (flag== false || numero>maximo)
+		{
+			maximo= numero;
+		}
+		if(flag== false || numero<minimo)
+		{
+			minimo=numero;
+			flag = true;
+		}
 
 
-}//FIN DE LA FUNCIÓN
+}
